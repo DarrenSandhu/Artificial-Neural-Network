3a79eb3fe64fbb311fb69a3442008704e444a4ea
Darren Sandhu <darrensandhu7@gmail.com>
2025-01-03 14:39:30 +0000

    Add additional weight init options

--- conv_neural_network/architecture/fully_connected_layer_torch.py
+++ conv_neural_network/architecture/fully_connected_layer_torch.py
@@ -6,18 +6,23 @@ class Fully_Connected_Layer():
         self.in_features = input_layer
         self.out_features = output_size
         self.weights = torch.empty(self.in_features, self.out_features)
-        if weight_init == "xavier":
-            torch.nn.init.xavier_normal_(self.weights)
-        elif weight_init == "xavier-uniform":
-            torch.nn.init.xavier_uniform_(self.weights)
-        elif weight_init == "kaiming-out":
-            torch.nn.init.kaiming_normal_(self.weights, mode='fan_out', nonlinearity='relu', a=0)
-        elif weight_init == "kaiming-in":
-            torch.nn.init.kaiming_normal_(self.weights, mode='fan_in', nonlinearity='relu', a=0)
-        elif weight_init == "kaiming-uniform":
-            torch.nn.init.kaiming_uniform_(self.weights, a=0)
+        if activation_func == "sigmoid":
+            torch.nn.init.xavier_normal_(self.kernels)
         else:
-            torch.nn.init.kaiming_normal_(self.weights, mode='fan_out', nonlinearity='relu', a=0)
+            if weight_init == "xavier":
+                torch.nn.init.xavier_normal_(self.weights)
+            elif weight_init == "xavier-uniform":
+                torch.nn.init.xavier_uniform_(self.weights)
+            elif weight_init == "kaiming":
+                torch.nn.init.kaiming_normal_(self.weights)
+            elif weight_init == "kaiming-out":
+                torch.nn.init.kaiming_normal_(self.weights, mode='fan_out', nonlinearity='relu', a=0)
+            elif weight_init == "kaiming-in":
+                torch.nn.init.kaiming_normal_(self.weights, mode='fan_in', nonlinearity='relu', a=0)
+            elif weight_init == "kaiming-uniform":
+                torch.nn.init.kaiming_uniform_(self.weights, a=0)
+            else:
+                torch.nn.init.kaiming_normal_(self.weights, mode='fan_out', nonlinearity='relu', a=0)
         # print("Fully Connected Weights: ", self.weights.shape)
         # print("\n")
 
